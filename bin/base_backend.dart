3e9926f43c6899e82a2f47f34e9018dbd67b35c1
Lucas Goulart <oi@lucasgoulart.com>
2024-02-17 18:22:32 -0300

    Configurar e validar conexão e database

--- bin/base_backend.dart
+++ bin/base_backend.dart
@@ -1,5 +1,24 @@
-import 'package:base_backend/base_backend.dart' as base_backend;
+import 'package:shelf/shelf.dart';
+import 'package:shelf/shelf_io.dart';
+import 'package:mysql1/mysql1.dart';
 
-void main(List<String> arguments) {
-  print('Hello world: ${base_backend.calculate()}!');
+void main(List<String> arguments) async {
+  final conn = await MySqlConnection.connect(ConnectionSettings(
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    db: 'delivery',
+    password: 'root',
+  ));
+
+  // await conn
+  //     .query("INSERT INTO tb_roles(name, status) values ('MANAGER', 'A') ");
+
+  print(await conn.query('select * from tb_roles'));
+
+  await serve(
+      (Request req) => Response(200,
+          body: 'Olá, mundo!', headers: {'content-type': 'application/json'}),
+      'localhost',
+      8081);
 }
